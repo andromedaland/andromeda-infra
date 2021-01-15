654751d98a2cabb91c7d1d8f005c9c3c484a114d
William Perron <hey@wperron.io>
2021-01-15 13:07:13 -0500

    bootstrap terraform infra

---
+++ meta.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "random_uuid" "this" {}
+data "aws_caller_identity" "this" {}
+
+locals {
+  prefix     = "andromeda"
+  short_uuid = substr(random_uuid.this.result, 0, 8)
+  tags = {
+    "deno.land/x:environment"    = var.env
+    "deno.land/x:uuid"           = local.short_uuid
+    "deno.land/x:provisioned-by" = reverse(split(":", data.aws_caller_identity.this.arn))[0]
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "${local.prefix}-terraform-state-${local.short_uuid}"
+  acl    = "private"
+  tags   = local.tags
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state_public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "state" {
+  name         = "${local.prefix}-terraform-lock-${local.short_uuid}"
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = local.tags
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "env" {
+  description = "short slug of the environment name"
+  type        = string
+}
+
+variable "region" {
+  description = "default aws region"
+  type        = string
+}
