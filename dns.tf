bf4e329a5f47d6978ee58bc9a67bbe5419501458
William Perron <hey@wperron.io>
2021-01-16 16:23:59 -0500

    add wildcard certificates for all domains

--- dns.tf
+++ dns.tf
@@ -2,10 +2,77 @@ data "aws_route53_zone" "parent" {
   name = "andromeda.land."
 }
 
+resource "aws_acm_certificate" "parent" {
+  domain_name       = "*.${data.aws_route53_zone.parent.name}"
+  validation_method = "DNS"
+  tags              = local.tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "parent_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.parent.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.parent.zone_id
+}
+
+resource "aws_acm_certificate_validation" "parent" {
+  certificate_arn         = aws_acm_certificate.parent.arn
+  validation_record_fqdns = [for record in aws_route53_record.parent_validation : record.fqdn]
+}
+
 resource "aws_route53_zone" "prod" {
   provider = aws.prod
   name     = "prod.andromeda.land."
   tags     = local.tags
+}
+
+resource "aws_acm_certificate" "prod" {
+  provider          = aws.prod
+  domain_name       = "*.${aws_route53_zone.prod.name}"
+  validation_method = "DNS"
+  tags              = local.tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "prod_validation" {
+  provider = aws.prod
+  for_each = {
+    for dvo in aws_acm_certificate.prod.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.prod.zone_id
+}
+
+resource "aws_acm_certificate_validation" "prod" {
+  provider                = aws.prod
+  certificate_arn         = aws_acm_certificate.prod.arn
+  validation_record_fqdns = [for record in aws_route53_record.prod_validation : record.fqdn]
 }
 
 resource "aws_route53_record" "prod_ns" {
@@ -25,6 +92,41 @@ resource "aws_route53_zone" "staging" {
   provider = aws.staging
   name     = "staging.andromeda.land."
   tags     = local.tags
+}
+
+resource "aws_acm_certificate" "staging" {
+  provider          = aws.staging
+  domain_name       = "*.${aws_route53_zone.staging.name}"
+  validation_method = "DNS"
+  tags              = local.tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "staging_validation" {
+  provider = aws.staging
+  for_each = {
+    for dvo in aws_acm_certificate.staging.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.staging.zone_id
+}
+
+resource "aws_acm_certificate_validation" "staging" {
+  provider                = aws.staging
+  certificate_arn         = aws_acm_certificate.staging.arn
+  validation_record_fqdns = [for record in aws_route53_record.staging_validation : record.fqdn]
 }
 
 resource "aws_route53_record" "staging_ns" {
